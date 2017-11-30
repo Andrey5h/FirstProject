5fa619c83a383916fcbafa7039be1925db4dd7b8
Andrey5h <maxhaste@yandex.ru>
2017-11-30 22:02:53 +0300

    Second commit

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -1,6 +1,18 @@
 public class Base {
     public static void main(String[] args)
     {
-        System.out.println("Helloy World");
+     /* Привет мир */
+        System.out.println("Hello, World!");
+        // Вычисление корня
+        int a = 675;
+        double root;
+        root = Math.sqrt(a);
+        System.out.printf("Корень числа %d равен %f", a, root);
+        //Возводим в степень
+        int b = 4;
+        int s = 3;
+        Math.pow(b,s);
+        System.out.printf("\nЧисло 4 в кубе = " +Math.pow(b,s));
+
     }
 }
